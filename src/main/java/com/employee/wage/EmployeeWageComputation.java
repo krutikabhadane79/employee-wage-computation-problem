e0b0b4aefb6818befa8a995534912700b1d17a79
Krutika Bhadane <krutikabhadane79@gmail.com>
2024-09-12 13:40:23 +0530

    Added: UC-2 Calculate daily employee wage

--- src/main/java/com/employee/wage/EmployeeWageComputation.java
+++ src/main/java/com/employee/wage/EmployeeWageComputation.java
@@ -6,14 +6,19 @@ public class EmployeeWageComputation
     {
         System.out.println( "Welcome to Employee Wage Computation Problem" );
         final int FULL_TIME = 1;
+        final int WAGE_PER_HR = 20;
+        int workingHours = 0;
         int empType = (int) (Math.random() * 100) % 2;
         if (empType == FULL_TIME)
         {
             System.out.println("Employee is Present");
+            workingHours = 8;
         }
         else
         {
             System.out.println("Employee is Absent");
         }
+        int wage = workingHours * WAGE_PER_HR;
+        System.out.println("Employee Daily Wage is " + wage);
     }
 }
