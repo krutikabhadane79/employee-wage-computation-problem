645b6477ba572b6d92ce034facacee283f9a091c
Krutika Bhadane <krutikabhadane79@gmail.com>
2024-09-12 14:15:43 +0530

    Added: UC-8 Compute employee wage for multiple companies

--- src/main/java/com/employee/wage/EmployeeWageComputation.java
+++ src/main/java/com/employee/wage/EmployeeWageComputation.java
@@ -2,18 +2,20 @@ package com.employee.wage;
 
 public class EmployeeWageComputation
 {
-    public static void calculateTotalWage()
+    public static void calculateTotalWage(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHrs)
     {
         final int FULL_TIME = 2;
         final int PART_TIME = 1;
-        final int WAGE_PER_HR = 20;
-        final int MAX_WORKING_DAYS = 20;
-        final int MAX_WORKING_HRS = 100;
         int totalWage = 0;
         int workingHours = 0;
+
+        System.out.println("Details of " + companyName + " employee");
+        System.err.println("Wage per hour:" + wagePerHr);
+        System.out.println("Maximum working days:" + maxWorkingDays);
+        System.out.println("Maximum working hours:" + maxWorkingHrs);
         System.out.printf("%5s     %5s     %5s     %5s\n", "Day", "Workinghrs", "Wage", "Total working hrs");
-        for (int day = 1, totalWorkingHrs = 0; day <= MAX_WORKING_DAYS
-                && totalWorkingHrs < MAX_WORKING_HRS; day++, totalWorkingHrs += workingHours)
+        for (int day = 1, totalWorkingHrs = 0; day <= maxWorkingDays
+                && totalWorkingHrs <= maxWorkingHrs; day++, totalWorkingHrs += workingHours)
         {
 
             int empType = (int) (Math.random() * 100) % 3;
@@ -27,15 +29,17 @@ public class EmployeeWageComputation
                 default:
                     workingHours = 0;
             }
-            int wage = workingHours * WAGE_PER_HR;
+            int wage = workingHours * wagePerHr;
             totalWage += wage;
             System.out.printf("%5d       %5d      %5d      %5d\n", day, workingHours, wage, totalWorkingHrs + workingHours);
         }
-        System.out.println("Total wage for month is " + totalWage);
+        System.out.println("Total wage for month is " + companyName + " employee is " + totalWage + "\n");
     }
 
     public static void main( String[] args ) {
         System.out.println("Welcome to Employee Wage Computation Problem");
-        calculateTotalWage();
+        calculateTotalWage("Amazon", 20,16, 150);
+        calculateTotalWage("Flipkart",40,20,100);
+        calculateTotalWage("Nykaa",30,15,100);
     }
 }
