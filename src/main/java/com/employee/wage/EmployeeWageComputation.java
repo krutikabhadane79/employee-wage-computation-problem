91aa294b11192f4770383e075b5a4bde0cca5cdc
Krutika Bhadane <krutikabhadane79@gmail.com>
2024-09-12 13:46:56 +0530

    Added: UC-3 Add part time employee and wage

--- src/main/java/com/employee/wage/EmployeeWageComputation.java
+++ src/main/java/com/employee/wage/EmployeeWageComputation.java
@@ -5,14 +5,20 @@ public class EmployeeWageComputation
     public static void main( String[] args )
     {
         System.out.println( "Welcome to Employee Wage Computation Problem" );
-        final int FULL_TIME = 1;
+        final int FULL_TIME = 2;
+        final int PART_TIME = 1;
         final int WAGE_PER_HR = 20;
         int workingHours = 0;
-        int empType = (int) (Math.random() * 100) % 2;
+        int empType = (int) (Math.random() * 100) % 3;
         if (empType == FULL_TIME)
         {
-            System.out.println("Employee is Present");
+            System.out.println("Employee is Present Full Time");
             workingHours = 8;
+        }
+        else if (empType == PART_TIME)
+        {
+            System.out.println("Employee is Present Part Time");
+            workingHours = 4;
         }
         else
         {
