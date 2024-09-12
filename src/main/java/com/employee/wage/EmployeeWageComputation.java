f3382b9656f98cb1c6dd1e7f74ddbd5547a2686b
Krutika Bhadane <krutikabhadane79@gmail.com>
2024-09-12 13:33:30 +0530

    Welcome to Employee Wage Computation Problem

---
+++ src/main/java/com/employee/wage/EmployeeWageComputation.java
@@ -0,0 +1,9 @@
+package com.employee.wage;
+
+public class EmployeeWageComputation
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Employee Wage Computation Problem" );
+    }
+}
