4e58a8ac032bb3d06d1818e214aefd64bc317495
Krutika Bhadane <krutikabhadane79@gmail.com>
2024-09-12 14:04:13 +0530

    Added: UC-6 Calculate wages till condition of total working hours or days is reached for a month

--- src/main/java/com/employee/wage/EmployeeWageComputation.java
+++ src/main/java/com/employee/wage/EmployeeWageComputation.java
@@ -8,26 +8,29 @@ public class EmployeeWageComputation
         final int FULL_TIME = 2;
         final int PART_TIME = 1;
         final int WAGE_PER_HR = 20;
-        final int WORKING_DAYS = 20;
+        final int MAX_WORKING_DAYS = 20;
+        final int MAX_WORKING_HRS = 100;
         int totalWage = 0;
-        for (int day = 1; day <= WORKING_DAYS; day++) {
-            int workingHours = 0;
+        int workingHours = 0;
+        System.out.printf("%5s     %5s     %5s     %5s\n", "Day", "Workinghrs", "Wage", "Total working hrs");
+        for (int day = 1, totalWorkingHrs = 0; day <= MAX_WORKING_DAYS
+                && totalWorkingHrs < MAX_WORKING_HRS; day++, totalWorkingHrs += workingHours)
+        {
+
             int empType = (int) (Math.random() * 100) % 3;
             switch (empType) {
                 case FULL_TIME:
-                    System.out.println("Employee is Present Full time");
                     workingHours = 8;
                     break;
                 case PART_TIME:
-                    System.out.println("Employee is Present Part time");
                     workingHours = 4;
                     break;
                 default:
-                    System.out.println("Employee is Absent");
+                    workingHours = 0;
             }
             int wage = workingHours * WAGE_PER_HR;
-            System.out.println("Employee Daily Wage is " + wage);
             totalWage += wage;
+            System.out.printf("%5d       %5d      %5d      %5d\n", day, workingHours, wage, totalWorkingHrs + workingHours);
         }
         System.out.println("Total wage for month is " + totalWage);
     }
